7e95da296938cebc99a9d1b37b65ca0843fecbef
Sharvari Wagh <sharvariwagh27@gmail.com>
2026-04-20 14:44:32 +0530

    Fix server port

--- backend/server.js
+++ backend/server.js
@@ -74,6 +74,6 @@ mongoose.connect(process.env.MONGO_URI)
 });
 
 console.log("SERVER STARTED");
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
